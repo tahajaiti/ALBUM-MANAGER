455e8840ca637fa78351959366c23d76b0b734dd
Taha <kyojin.rar@gmail.com>
2024-12-21 13:34:03 +0100

    FEAT: added buy history model to fetch and return user purchase history

---
+++ model/buy_history.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+include_once '../includes/db.php';
+
+try {
+    $query = "SELECT 
+            p.user_id AS uId, 
+            p.purchase_date AS pDate, 
+            a.title,
+            a.cover_image AS cover,
+            a.price
+            p.album_id AS aId, 
+        FROM purchases p
+        JOIN albums a 
+        ON p.album_id = a.id
+        WHERE p.user_id = :id;
+";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([":id" => $_SESSION['user_id']]);
+    $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($row);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["status" => False ,"error" => "Error fetching purchase history: " . $e->getMessage()]);
+    exit();
+}
