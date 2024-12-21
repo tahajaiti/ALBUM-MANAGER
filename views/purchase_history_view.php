d793c5d27f11081a26c7607f3039dd6a09d953c4
Taha <kyojin.rar@gmail.com>
2024-12-21 13:14:03 +0100

    FEAT: added purchase history view with a table layout for displaying user purchases

---
+++ views/purchase_history_view.php
@@ -0,0 +1,25 @@
+<main>
+    <section class="mt-8 p-16">
+        <h2 class="text-3xl font-bold mb-6">Purchase History</h2>
+        <div class="bg-gray-800/50 backdrop-blur-xl rounded-sm overflow-hidden">
+            <table class="w-full">
+                <thead>
+                    <tr class="bg-gray-700/50 backdrop-blur-md">
+                        <th class="px-6 py-3 text-left">Cover</th>
+                        <th class="px-6 py-3 text-left">Title</th>
+                        <th class="px-6 py-3 text-left">Purchased At</th>
+                        <th class="px-6 py-3 text-left">Quantity</th>
+                        <th class="px-6 py-3 text-left">Price</th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody">
+                        <td>
+                        <p>No purchases yet.</p>
+                        </td>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</main>
+
+<script type="module" src="./dist/purchase_history.js"></script>
